af0288793562d12fea6f78c9811c4f3bdd74a1ff
Victos420 <75263960+Victos420@users.noreply.github.com>
2021-01-24 02:01:22 +0700

    Add files via upload

---
+++ 2nd-year/c-labs/lab1/cmake-build-debug/CMakeFiles/lab1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab1.dir/main.cpp.o"
+  "CMakeFiles/lab1.dir/tests.cpp.o"
+  "CMakeFiles/lab1.dir/tritset.cpp.o"
+  "lab1.exe"
+  "lab1.pdb"
+  "liblab1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
